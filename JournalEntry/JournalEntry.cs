d78d262edbcae98b4ac44cdf51c2bcdf03714f01
Stephen Hollingsworth <stevh177@yahoo.com>
2020-12-17 10:23:43 -0800

    Add very basic log to a file, more of an error file

--- JournalEntry/JournalEntry.cs
+++ JournalEntry/JournalEntry.cs
@@ -43,9 +43,14 @@ namespace JournalEntry
                         fileStream.Write(headerTextBytes);
                     }
                 }
+                else
+                {
+                    LogJournalCreation();
+                }
             }
             catch (Exception ex)
             {
+                LogJournalCreation(true);
                 return EntryCreationStatus.Failure;
             }
             return EntryCreationStatus.Success;
@@ -65,5 +70,43 @@ namespace JournalEntry
         {
             throw new NotImplementedException();
         }
+
+        // TODO: Reconsider how to do this
+        public bool LogJournalCreation(bool failure = false)
+        {
+            // Get the path for the log
+            string journalDirectory = $"C:\\Users\\Stephen\\Documents\\Writing\\Journal";
+
+            // TODO: Get the write Date format
+            string today = DateTime.Now.ToString("MMMdd_yyyy");
+
+            string statusMsg = "FailedToCreateEntry";
+
+            string filename = $"{statusMsg}_{today}.md";
+
+            string fullPath = $"{journalDirectory}\\{filename}";
+
+            string headerText = failure ? "Unknown error" : "Journal entry already present"; // GetHeaderText();
+
+            try
+            {
+                if (!File.Exists(fullPath))
+                {
+                    using (var fileStream = System.IO.File.Create(fullPath))
+                    {
+                        byte[] headerTextBytes = new UTF8Encoding(true).GetBytes(headerText);
+
+                        fileStream.Write(headerTextBytes);
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return false;
+        }
+
     }
 }
